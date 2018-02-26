9dfa4612e8a2e4cb477574c6ae12c7e0aa5c7a29
tamirbenCA <33455948+tamirbenCA@users.noreply.github.com>
2018-02-26 14:05:52 +0200

    Update homePage.js

--- cmps/homePage.js
+++ cmps/homePage.js
@@ -57,7 +57,7 @@ export default {
     },
     created() {
         surveyService.initExp();
-        console.log(this.$route.query);
+        console.log(this.$route);
         if (this.$route.query.secret) {
             this.showPage = true;
         }
